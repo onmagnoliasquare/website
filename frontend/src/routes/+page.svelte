a754acce5c5f8313785adfdbcc473381d8d8effa
tinakralimarkova1 <67720045+tinakralimarkova1@users.noreply.github.com>
2024-03-12 10:01:43 +0800

    feat(api-connection): implement front-end integration with back-end services

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<article>
+	<header>
+		<h1>Sanity + SvelteKit</h1>
+	</header>
+	<main>
+		<h2>Article</h2>
+		{#if data.article && data.article.length}
+		<ul>
+			{#each data.article as article}
+			<li>{article.name}</li>
+			{/each}
+		</ul>
+		{:else}
+		<p>No article found.</p>
+		{/if}
+		<div>
+			{#if data.article && data.article.length}
+			<pre>
+			{JSON.stringify(data.article, null, 2)}
+  	  </pre>
+			{:else}
+			<p>Your data will show up here when you've configured everything correctly</p>
+			{/if}
+		</div>
+	</main>
+</article>
+
+<script>
+	export let data;
+</script>
+
