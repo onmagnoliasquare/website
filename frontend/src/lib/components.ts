5430a7b07f873647bf540ef781d0b525cbd9b7c6
n30w <hello@neoalabastro.com>
2024-03-15 22:47:53 +0800

    feat: add portable text support

---
+++ frontend/src/lib/components.ts
@@ -0,0 +1,7 @@
+/**
+ * Contains the custom component renderings for Portable Text.
+ * See this link for more details:
+ * https://www.sanity.io/docs/portable-text-to-react#customizing-components
+ */
+
+export const customComponents = {};
