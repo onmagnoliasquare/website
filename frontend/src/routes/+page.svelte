637e67156cc97157d81b6fe29d66d16d345d532c
tinakralimarkova1 <67720045+tinakralimarkova1@users.noreply.github.com>
2024-03-15 17:40:24 +0800

    update: api layer

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,33 @@
-<article>
+<script>
+export let data;
+console.log(data);
+let article = data.articles
+console.log("hello");
+
+</script>
 	<header>
 		<h1>Sanity + SvelteKit</h1>
 	</header>
 	<main>
 		<h2>Article</h2>
-		{#if data.article && data.article.length}
+
+		<ul>
+			{#each article as article}
+			<a href="/article/{article.name.current}">Read more about {article.title}</a>
+			{/each}
+
+		</ul>
+
+
+	</main>
+
+
+
+
+
+	<!-- <main>
+		<h2>Article</h2>
+		{#if article && data.article.length}
 		<ul>
 			{#each data.article as article}
 			<li>{article.name}</li>
@@ -19,13 +42,18 @@
 			{JSON.stringify(data.article, null, 2)}
   	  </pre>
 			{:else}
-			<p>Your data will show up here when you've configured everything correctly</p>
+			<ul>
+				{#each data.article as article}
+				<li>{article.name}</li>
+				{/each}
+
+			</ul>
 			{/if}
 		</div>
 	</main>
 </article>
 
 <script>
-	export let data;
+	export let article;
 </script>
-
+ -->
