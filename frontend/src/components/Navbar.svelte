9743eb9c092b931c90aa21d1f6d3ea501c08d781
n30w <rocketneo@gmail.com>
2024-03-25 17:48:57 +0800

    feat: add basic navbar

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/opinion">Opinion</a></li>
+        <li><a href="/people">People</a></li>
+        <li><a href="/culture">Culture</a></li>
+        <li><a href="/professors">Professors</a></li>
+        <li><a href="/multimedia">Multimedia</a></li>
+        <li><a href="/series">Series</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+    <form id="form">
+        <input type="search" id="query" name="q" placeholder="Search..." aria-label="Search through site content">
+        <button>Search</button>
+    </form>
+</nav>
+
+<style>
+    nav, ul, li, form {
+        display: inline;
+    }
+</style>
