e813ac3bafb7e73fe935fff5d9cc9ab110c67c92
n30w <rocketneo@gmail.com>
2024-03-25 13:16:15 +0800

    feat: add normalize.css

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
+<script>
+	import '../normalize.css'
+</script>
+
 <!-- This is the main layout for the entire website. -->
-<div class="container">
-	<header>On Magnolia Square</header>
-	<main>
-		<slot />
-	</main>
-	<footer>
-		<p>On Magnolia Square</p>
-	</footer>
-</div>
+<header>On Magnolia Square</header>
+<main>
+	<slot />
+</main>
+<footer>
+	<p>On Magnolia Square</p>
+</footer>
