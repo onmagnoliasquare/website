16362c84b95b5c47bd6e4112e7dba4bdae5c1eae
n30w <rocketneo@gmail.com>
2024-03-26 21:22:13 +0800

    feat: conditional rendering support for subtitle

--- frontend/src/routes/category/[category=categories]/[slug]/+page.svelte
+++ frontend/src/routes/category/[category=categories]/[slug]/+page.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
-    import { PortableText } from '@portabletext/svelte';
-    import type { PageData } from './$types';
-    import { customComponents } from '$lib/components';
 	import { PortableText } from '@portabletext/svelte';
 	import type { PageData } from './$types';
 	import { customComponents } from '$lib/components';
 	import SingleArticleBlock from '../../../../components/portabletext/SingleArticleBlock.svelte';
 
-    export let data: PageData;
 	export let data: PageData;
 </script>
 
-<h2>{data.article.title}</h2>
-<h3>{data.article.subtitle}</h3>
 <h1>{data.article.title}</h1>
-<h2>{data.article.subtitle}</h2>
+{#if data.article.subtitle}
+	<h2>{data.article.subtitle}</h2>
+{/if}
 <ul>
-    <li>{data.article.date}</li>
-    <li>{data.article.authors[0].name}</li>
 	<li>{data.article.date}</li>
 	<li>{data.article.authors[0].name}</li>
 </ul>
 
 <article>
-    <PortableText components={customComponents} value={data.article.content} />
 	<PortableText
 		components={{
 			block: SingleArticleBlock
@@ -44,25 +37,45 @@
 {/if}
 
 <style>
-    /* Code modified from:  */
-    /* https://css-tricks.com/almanac/properties/h/hyphenate/ */
-    /* Can also use this library if need be: */
-    /* https://github.com/mnater/Hyphenopoly */
-    article {
-        max-width: 500px;
-        margin: 0 auto;
-        width: 100%;
-        text-align: justify;
-        font-size: 20px;
-        line-height: 144%;
+	/* Code modified from:  */
+	/* https://css-tricks.com/almanac/properties/h/hyphenate/ */
+	/* Can also use this library if need be: */
+	/* https://github.com/mnater/Hyphenopoly */
+	article {
+		max-width: 500px;
+		margin: 0 auto;
+		width: 100%;
+		/* text-align: justify; */
+	}
 
-        font-family: 'Archivo Regular';
-    }
+	h1 {
+		margin: 0 auto;
+		width: 100%;
+		font-size: 5.6875rem;
+		letter-spacing: -0.34125rem;
+		font-weight: 200;
+		line-height: 112%;
+		font-family: 'Noto Serif Regular';
+	}
 
-    article p {
-        -webkit-hyphens: auto;
-        -moz-hyphens: auto;
-        -ms-hyphens: auto;
-        hyphens: auto;
-    }
+	h2 {
+		margin: 0 auto;
+		width: 100%;
+		font-size: 3.8125rem;
+		letter-spacing: -0.1525rem;
+		font-weight: 300;
+		font-variation-settings: 'wdth' 100;
+		font-family: 'Noto Serif Regular';
+	}
+
+	h3 {
+		margin: 0 auto;
+		width: 100%;
+		font-size: 8.5625rem;
+		letter-spacing: -0.1525rem;
+		font-weight: 300;
+		line-height: 96%;
+		font-variation-settings: 'wdth' 88;
+		font-family: 'Noto Serif Regular';
+	}
 </style>
