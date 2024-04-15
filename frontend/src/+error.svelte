86ca03dc9038d89602abea789eb24087bb3bc31b
n30w <rra9981@nyu.edu>
2024-04-15 12:02:14 +0800

    feat: add +error.svelte

---
+++ frontend/src/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export const msg: string = $page.error?.message as string;
+	export const status: number = $page.status;
+</script>
+
+<!-- TODO center this message! -->
+<h1>{status}: {msg}</h1>
+<h2>Looks like something went wrong...</h2>
+<p><a href="/">Click here to go back to the homepage</a></p>
