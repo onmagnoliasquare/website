2aeeb3fb34e64df386fc11397446a919faaa7a99
n30w <rra9981@nyu.edu>
2024-04-17 16:27:47 +0800

    test(back): add slugValidator

---
+++ packages/backend/lib/index.test.ts
@@ -0,0 +1,73 @@
+import {describe, it, expect, test} from 'vitest'
+import slugValidator from './slugValidator'
+
+describe('slugValidator', () => {
+  // Uppercase to lowercase.
+  const upperToLower = [
+    ['ABCDEFG', 'abcdefg'],
+    ['ABCdefg', 'abcdefg'],
+    ['abcDEFg', 'abcdefg'],
+    ['AbCdEfG', 'abcdefg'],
+  ]
+
+  // Replaces uppercase to lowercase.
+
+  // Replaces special characters.
+  const specialChars = [
+    ['ab!cDEfg', 'abcdefg'],
+    ['A!??bcd', 'abcd'],
+    ['.&.aB#?130', 'ab130'],
+  ]
+
+  // Replaces spaces with hyphens.
+  const spaceToHyphen = [
+    ['hello world', 'hello-world'],
+    ['THIS IS A TEST.', 'this-is-a-test'],
+    ['HELLO! Go ahead, ILLEGAL ch#!', 'hello-go-ahead-illegal-ch'],
+  ]
+
+  // Replaces all types of brackets.
+  const replaceBrackets = [
+    [`{this type of thing isn't allowed`, `this-type-of-thing-isnt-allowed`],
+    [`[when and where]? Did you find this?`, `when-and-where-did-you-find-this`],
+    [`THE WORLD is_round...`, `the-world-is_round`],
+  ]
+
+  // Removes *common* diacritics. There will be edge cases not accounted for.
+  const removeDiacritics = [
+    [`áēîöùćž`, `aeioucz`],
+    [`ěûäõșţ`, `euaost`],
+    [`iîøüçñĝ`, `iiøucng`],
+  ]
+
+  // Truncates output to max 200 characters.
+  const charLimit = [
+    [
+      `The experiment was to see if NYU Shanghai could sustain free metal cups on campus provided for when students wanted to drink water. These cups were available at the café, but could also be taken throughout the school, if they were later returned.`,
+      `the-experiment-was-to-see-if-nyu-shanghai-could-sustain-free-metal-cups-on-campus-provided-for-when-students-wanted-to-drink-water-these-cups-were-available-at-the-cafe-but-could-also-be-taken-through`,
+    ],
+    [
+      `Beginning as a collaboration between the NYU Shanghai Writing Department and students of NYU Shanghai’s Inaugural Class, On Magnolia Square, formerly On Century Avenue, has bloomed into a thriving daily student publication`,
+      `beginning-as-a-collaboration-between-the-nyu-shanghai-writing-department-and-students-of-nyu-shanghais-inaugural-class-on-magnolia-square-formerly-on-century-avenue-has-bloomed-into-a-thriving-daily-s`,
+    ],
+    [
+      `“I always had this sense of Los Angeles, but I never knew what it was about,” Louise said. “So, I was curious, because this movie looked like it could give me a new perspective.” “I think the [cinema] atmosphere definitely helped because if it was in a classroom or something it would be very different. This actually made you get into the movie and zone out of the world,” she said.`,
+      `i-always-had-this-sense-of-los-angeles-but-i-never-knew-what-it-was-about-louise-said-so-i-was-curious-because-this-movie-looked-like-it-could-give-me-a-new-perspective-i-think-the-cinema-atmosphere-d`,
+    ],
+  ]
+
+  // JS Spread Syntax
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  test.each([
+    ...upperToLower,
+    ...specialChars,
+    ...spaceToHyphen,
+    ...replaceBrackets,
+    ...removeDiacritics,
+    ...charLimit,
+  ])('%s -> %s', (input, output) => {
+    expect(slugValidator(input)).toBe(output)
+  })
+})
+
+// describe('slugValidator', () => {})
--- packages/backend/lib/slugValidator.ts
+++ packages/backend/lib/slugValidator.ts
@@ -1,11 +1,26 @@
 // Solved with: https://chat.openai.com/share/79be13ea-801d-47ab-b2f5-695be8a58430
 export default function slugValidator(slug: string): string {
   const newSlug = slug
+    // Make everything lowercase.
     .toLowerCase()
-    .replace(/[!(),.\\$%&]/g, '') // Replacing special characters with nothing
-    .replace(/\s+/g, '-') // Replacing spaces with hyphens
-    .replace(/[\[\]{}]/g, '') // Replacing all types of brackets
-    .slice(0, 200) // Ensuring the slug is no longer than 200 characters
+
+    // Replace special characters with nothing.
+    // Quotation marks retrieved from: https://en.wikipedia.org/wiki/Quotation_mark
+    .replace(/[!(),.\\$%&?#'"“”‘’「」「」《》«»]/g, '')
+
+    // Replace spaces with hyphens.
+    .replace(/\s+/g, '-')
+
+    // Replace all types of brackets.
+    .replace(/[\[\]{}]/g, '')
+
+    // Remove common diacritics. See: https://stackoverflow.com/a/37511463/20087581
+    // Of course, there are some edge cases for this, but this should cover most practically.
+    .normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+
+    // Ensuring the slug is no longer than 200 characters.
+    .slice(0, 200)
 
   return newSlug
 }
