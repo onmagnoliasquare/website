92024f68c28a8f3a4f47b8540269067e0c89e2de
n30w <rra9981@nyu.edu>
2024-07-04 22:08:31 -1000

    feat: portable text impl

---
+++ packages/frontend/src/lib/defaultComponents/DefaultList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { ListComponentProps } from '../rendererTypes';
+
+	export let portableText: ListComponentProps;
+
+	$: ({ value } = portableText);
+
+	$: ({ listItem } = value);
+</script>
+
+{#if listItem === 'number'}
+	<ol><slot /></ol>
+{:else}
+	<ul><slot /></ul>
+{/if}
+
+<style>
+	ol,
+	ul {
+		font-family: 'Archivo Regular';
+	}
+</style>
--- packages/frontend/src/lib/index.ts
+++ packages/frontend/src/lib/index.ts
@@ -10,3 +10,27 @@ export { dateFormatter, hasUppercase };
 
 // Components
 export { Navbar, ArticleHardBreak, SingleArticleBlock };
+
+// Main module users interact with
+export { default as PortableText } from './PortableText.svelte';
+
+// Utilities
+export { toPlainText } from '@portabletext/toolkit';
+
+// Default components for edge cases of overwriting components in a specific way
+export { default as DefaultBlock } from './defaultComponents/DefaultBlock.svelte';
+export { default as DefaultList } from './defaultComponents/DefaultList.svelte';
+export { default as DefaultListItem } from './defaultComponents/DefaultListItem.svelte';
+export { default as DefaultMark } from './defaultComponents/DefaultMark.svelte';
+
+// Types
+export type { InputValue } from './ptTypes';
+
+export type {
+	BlockComponentProps,
+	CustomBlockComponentProps,
+	MarkComponentProps,
+	ListComponentProps,
+	ListItemComponentProps,
+	PortableTextComponents
+} from './rendererTypes';
