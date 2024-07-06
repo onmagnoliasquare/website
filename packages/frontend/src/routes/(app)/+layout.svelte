e35ebf2a0350305c9ea8ea1d01dafdbd59418eef
n30w <rra9981@nyu.edu>
2024-07-06 03:55:46 -1000

    feat: add `createSiteTitle` to main layout

--- packages/frontend/src/routes/(app)/+layout.svelte
+++ packages/frontend/src/routes/(app)/+layout.svelte
@@ -4,7 +4,20 @@
 	 * */
 
 	import { Navbar } from '$lib';
+	import { page } from '$app/stores'
+	import createSiteTitle from '$lib/createSiteTitle';
+
+	/**
+	 * This documentation discusses $page.data and how to access it
+	 * from the layout.
+	 * https://kit.svelte.dev/docs/load#$page-data
+	 * */
+
 </script>
+
+<svelte:head>
+	<title>{createSiteTitle($page.data.title)}</title>
+</svelte:head>
 
 <!-- This is the main layout for the entire website. -->
 <header>
