27bf2731269bb5a7980c0c44f5fd3f7f388dfac1
n30w <99268950+n30w@users.noreply.github.com>
2024-09-10 03:41:46 +0800

    refactor: change `member` fields to optional
    Only `name` is required.

--- packages/backend/schemaTypes/member.ts
+++ packages/backend/schemaTypes/member.ts
@@ -2,7 +2,19 @@ import {UserIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
 
 /**
- * A "member" is someone who writes for our organization.
+ * A "member" is someone who produces content for our organization.
+ *
+ * Member data is not fully opt-in. The only required field is the
+ * `name`. This is important because our dataset is not
+ * guaranteed to be private and so storing personal information
+ * online without safeguard is a privacy risk.
+ *
+ * Documents and records regarding personal contact information can
+ * be stored on an internal document or database, like Notion or
+ * Google Workspace (ironically these aren't that privacy respecting
+ * either, but its more a matter of who gets to see what).
+ *
+ * Members should have free-will to choose what they want to expose.
  */
 
 export default defineType({
@@ -16,21 +28,33 @@ export default defineType({
       title: 'Name',
       type: 'string',
       description: 'First Name and Last Name',
+
+      // This is the only required field, as it links
+      // an author to their article.
       validation: (rule) => rule.required(),
     }),
 
     defineField({
       name: 'year',
-      title: 'Graduation Year',
+      title: 'Joined Year',
       type: 'number',
-      validation: (rule) => rule.required().integer().min(2013),
+
+      // The only valid inputs would be the organization's founding up to the current year.
+      validation: (rule) => rule.integer().min(2013).max(new Date().getFullYear()),
     }),
 
     defineField({
       name: 'netid',
       title: 'NYU Net ID',
       type: 'string',
-      validation: (rule) => rule.required(),
+
+      // This is optional–way back when,
+      // we used to keep this in the Wordpress DB
+      // since that was private. Now that the guarantee
+      // of a private dataset is near non-existent, we must
+      // conceal data appropriately.
+      //
+      // validation: (rule) => rule.required(),
     }),
 
     defineField({
@@ -50,7 +74,7 @@ export default defineType({
         // https://www.sanity.io/docs/image-type#hotspot-3e6da78954a8
         hotspot: true,
       },
-      description: 'Optional profile picture to display on the website',
+      description: '⚠️ WIP ⚠️ Optional profile picture to display on the website',
     }),
 
     defineField({
