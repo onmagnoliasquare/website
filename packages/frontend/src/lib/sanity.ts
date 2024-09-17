ab1df9dee69a54d5416fb8eb454a7b21e5d9305e
n30w <99268950+n30w@users.noreply.github.com>
2024-09-18 05:19:21 +0800

    feat: sanity query for category

--- packages/frontend/src/lib/sanity.ts
+++ packages/frontend/src/lib/sanity.ts
@@ -46,7 +46,10 @@ export { client };
  * @async
  */
 export async function getArticles(): Promise<Article[]> {
-	return await client.fetch(groq`*[_type == "article"] | order(_createdAt desc)`);
+	return await client.fetch(
+		groq`*[_type == "article"]{ title,
+		category->{name}, slug } | order(_createdAt desc)`
+	);
 }
 
 /**
@@ -357,10 +360,11 @@ export async function getArticleTags(slug: string): Promise<Article> {
 export interface Member {
 	_type: 'member';
 	name: string;
-	year: number;
-	netid: string;
-	bio: string;
-	portrait: ImageAsset;
+	year?: number;
+	netid?: string;
+	bio?: string;
+	portrait?: ImageAsset;
+	// handles
 }
 
 export interface Tag {
