4f9a6262ecc72095dbacd10ae66b74883825ecf6
n30w <99268950+n30w@users.noreply.github.com>
2024-09-26 02:54:08 +0800

    chore: update English errors in `ProtoAnnouncement`

--- packages/frontend/src/components/home/ProtoAnnouncement.svelte
+++ packages/frontend/src/components/home/ProtoAnnouncement.svelte
@@ -7,10 +7,10 @@
 		<div class="pa3">
 			<p class="fw8 ma0 f4 f3-l pa0">Our website is under active development 🚧</p>
 			<p class="tracked-02 ma0 f6 f5-ns pa0 pt1 pt3-ns">
-				After the complications with GoDaddy's wordpress hosting solutions, we had no choice but to
-				push a development release of the website. Some things may not work and some stylings may
-				look unfinished and a lot of content may be missing, but core functionality is present. So
-				read at your leisure!
+				After complications and issues with GoDaddy's wordpress hosting solutions, we had no choice
+				but to push a development release of the new website. Some things may not work and some
+				stylings may look unfinished and a lot of content may be missing, but core functionality is
+				present. Any further updates will be on a rolling basis. Feel free to read at your leisure.
 			</p>
 		</div>
 	</div>
