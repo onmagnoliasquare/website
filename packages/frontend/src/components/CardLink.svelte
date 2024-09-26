74cafc4b53248c7bc4f1442edc2fd529cbf536aa
n30w <99268950+n30w@users.noreply.github.com>
2024-09-26 23:44:11 +0800

    feat(f): general+about+cardLink components

---
+++ packages/frontend/src/components/CardLink.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let title;
+	export let link;
+	export let bgColor = 'purple';
+</script>
+
+<div class="mw6 ba border-color w-100 ma2" id={`${bgColor}`}>
+	<a href={link} class="ma0 pa0">
+		<div class="flex flex-column">
+			<div class="pl4 pt4 pb4">
+				<p class="pa0 ma0 f1 newSerif fw5">{title}</p>
+			</div>
+			<div class="pl4 pb4">
+				<p class="pa0 ma0 f5 tracked-02"><slot /></p>
+			</div>
+		</div>
+	</a>
+</div>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+
+	#green:hover {
+		background-color: var(--light-green);
+	}
+
+	#purple:hover {
+		background-color: var(--light-purple);
+	}
+</style>
---
+++ packages/frontend/src/components/general/VersionLabel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let version = import.meta.env.VITE_RELEASE_VERSION;
+</script>
+
+<div class="ph3">
+	<p class="f6 pa0 ma0 font-monospace tracked-tight gray">
+		v{version}
+	</p>
+</div>
