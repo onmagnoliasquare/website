c80996a02f7cb4b938bcac1e05b431f4621c0675
n30w <99268950+n30w@users.noreply.github.com>
2024-09-27 00:18:39 +0800

    refactor(f): update announcement content+styling

--- packages/frontend/src/components/home/ProtoAnnouncement.svelte
+++ packages/frontend/src/components/home/ProtoAnnouncement.svelte
@@ -3,15 +3,28 @@
 </script>
 
 <div class="ma2 mv3">
-	<div class="ba border-color mw5 mw9 center">
+	<div class="ba border-color mw5 mw9 center" id="mainBorder">
 		<div class="pa3">
-			<p class="fw8 ma0 f4 f3-l pa0">Our website is under active development 🚧</p>
+			<p class="w-100 fw8 ma0 f5 f3-l pa0 tc">🚧 Site under active development 🚧</p>
+
 			<p class="tracked-02 ma0 f6 f5-ns pa0 pt1 pt3-ns">
-				After complications and issues with GoDaddy's wordpress hosting solutions, we had no choice
-				but to push a development release of the new website. Some things may not work and some
-				stylings may look unfinished and a lot of content may be missing, but core functionality is
-				present. Any further updates will be on a rolling basis. Feel free to read at your leisure.
+				Complications required a release of a rolling development version of our site. Some
+				function, styling, and content may be absent or unfinished, but core functionality is
+				present. Read at your leisure.
 			</p>
 		</div>
 	</div>
 </div>
+
+<style>
+	#mainBorder {
+		border-style: dashed;
+		opacity: 50%;
+		transition: all 0.2s ease-out;
+	}
+
+	#mainBorder:hover {
+		opacity: 100%;
+		/* transition: all 200ms cubic-bezier(0.075, 0.82, 0.165, 1); */
+	}
+</style>
