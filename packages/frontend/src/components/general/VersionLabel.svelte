277b86609dbae7952374de52a07e09eaa0241569
n30w <99268950+n30w@users.noreply.github.com>
2024-09-28 02:07:57 +0800

    refactor(f): import `semVer` from `$lib`

--- packages/frontend/src/components/general/VersionLabel.svelte
+++ packages/frontend/src/components/general/VersionLabel.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	let version = import.meta.env.VITE_RELEASE_VERSION;
+	import { semVer } from '$lib/variables';
+	// let version = import.meta.env.VITE_RELEASE_VERSION;
 </script>
 
 <div class="ph3">
 	<p class="f6 pa0 ma0 font-monospace tracked-tight gray">
-		v{version}
+		v{semVer}
 	</p>
 </div>
