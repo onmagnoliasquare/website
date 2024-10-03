04fffa25c499438a8c97253b1e28c2b4f48c9a54
Neo <99268950+n30w@users.noreply.github.com>
2024-10-03 19:36:29 +0800

    refactor(f): `ArticleImage` spacing+title placement

--- packages/frontend/src/components/portabletext/ArticleImage.svelte
+++ packages/frontend/src/components/portabletext/ArticleImage.svelte
@@ -5,23 +5,27 @@
 </script>
 
 <figure class="ma2 mb4">
-	{#if portableText.value.attrs.title}
-		<h4 class="tc fw3">{portableText.value.attrs.title}</h4>
-	{/if}
-
 	<!-- If there's a description, no need altText -->
 	{#if portableText.value.attrs.description}
-		<img src={urlFor(portableText.value).fit('max').auto('format').url()} alt="" loading="lazy" />
+		<img
+			src={urlFor(portableText.value).fit('max').auto('format').url()}
+			alt={portableText.value.attrs.description}
+			loading="lazy"
+		/>
 	{/if}
-
+	{#if portableText.value.attrs.title}
+		<h4 class="ph1 fw5 i ma0 mb3 mt2">{portableText.value.attrs.title}</h4>
+	{/if}
 	{#if portableText.value.attrs.description || portableText.value.attrs.creditLine}
-		<figcaption class="fw5 f6 gray">
+		<figcaption class="fw5 f6 gray ph1">
 			<div class="flex flex-column">
 				<div>
 					{portableText.value.attrs.description}
 				</div>
-				<div class="o-50 mt1">
-					Photo Credit: {portableText.value.attrs.creditLine}
+				<div class="mt1">
+					<p class="pa0 ma0 i o-40">
+						Photo credit: {portableText.value.attrs.creditLine}
+					</p>
 				</div>
 			</div>
 		</figcaption>
