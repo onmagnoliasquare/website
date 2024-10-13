af5dfa82b0c23a265f746b783516640827c2d870
Neo <99268950+n30w@users.noreply.github.com>
2024-10-13 20:35:49 +0800

    refactor(f): use `CategoryArticleBox`

--- packages/frontend/src/routes/(app)/category/[category=categories]/+page.server.ts
+++ packages/frontend/src/routes/(app)/category/[category=categories]/+page.server.ts
@@ -6,7 +6,7 @@ import createSiteTitle from '$lib/createSiteTitle';
 export const load: PageServerLoad = (async (event: ServerLoadEvent) => {
 	const { category } = event.params!;
 	const articles: Article[] = await getArticlesFromCategory(category as string);
-	const title = category!.charAt(0).toUpperCase() + category!.slice(1)
+	const title = category!.charAt(0).toUpperCase() + category!.slice(1);
 
 	if (articles) {
 		return {
