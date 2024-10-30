a54cfb4daefbf4f6e46715584a2f6600ee7e28ed
Neo <99268950+n30w@users.noreply.github.com>
2024-10-31 03:59:43 +0800

    feat(f): add contributor about page
    This is a member page and shows bio, contacts, name, picture, and
    articles written.

---
+++ packages/frontend/src/components/general/ContactIcons.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let link;
+	export let title;
+	export let icon;
+</script>
+
+<div class="ma0 pa0">
+	<a href={link} target="_blank" class="no-underline">
+		<div class="flex items-center">
+			<img src={icon} alt="" class="di pa0 ma0 o-50" width={20} height={20} />
+			<p class="tracked-02 fw6 f6 di pa1 ma0 gray">{title}</p>
+		</div>
+	</a>
+</div>
--- packages/frontend/src/components/home/ArticleBoxC.svelte
+++ packages/frontend/src/components/home/ArticleBoxC.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { urlFor } from '$lib/sanity';
+	import Image from '$components/Image.svelte';
 	import ByLine from './ByLine.svelte';
 	import DateLine from './DateLine.svelte';
 
@@ -9,12 +9,12 @@
 <a
 	data-sveltekit-preload-code="viewport"
 	data-sveltekit-preload-data="tap"
-	href={`/category/${article.category.name}/${article.slug.current}`}
+	href={`/category/${article.category.name.toLowerCase()}/${article.slug.current}`}
 	class="dim"
 >
 	<article class="pv4 bt bb b--black-10 ph3 ph0-l">
-		<div class="flex flex-column flex-row-ns">
-			<div class="w-100 w-60-ns pr3-ns order-2 order-1-ns">
+		<div class="flex flex-column flex-row-l">
+			<div class={`w-100 ${article.media ? `w-60-l` : ``} pr3-l order-2 order-1-l`}>
 				<h1 class="f3 mt0 lh-title">
 					{article.title}
 				</h1>
@@ -24,11 +24,11 @@
 					</p>
 				{/if}
 			</div>
-			<div class="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-				{#if article.media}
-					<img class="db" src={urlFor(article.media).format('webp').fit('max').url()} alt="" />
-				{/if}
-			</div>
+			{#if article.media}
+				<div class="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-l">
+					<Image media={article.media} width={480} height={320} quality={20} fit={'crop'} />
+				</div>
+			{/if}
 		</div>
 		<ByLine authors={article.authors} />
 		<DateLine date={article.date} />
