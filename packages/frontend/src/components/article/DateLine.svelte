f416f56eb18d0201b5064a23351ffe167fcc00d1
Neo <99268950+n30w@users.noreply.github.com>
2024-11-01 02:40:28 +0800

    fix(f): enforce `navigator` render on client
    Workaround for reload error "navigator is undefined".

--- packages/frontend/src/components/article/DateLine.svelte
+++ packages/frontend/src/components/article/DateLine.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import { dateFormatter } from '$lib/helpers';
+	import { onMount } from 'svelte';
+
+	let userAgent: Navigator;
+
+	// This is a workaround for the error "navigator is undefined"
+	onMount(() => {
+		userAgent = navigator;
+	});
 
 	export let date: string;
 </script>
 
 <p class="pa0 ma0 lh-copy tracked-02 fw4 f6">
-	<time datetime={date}>{dateFormatter(date, navigator.language)} UTC</time>
+	<!-- This is a workaround for "cannot read undefined" -->
+	{#if userAgent !== undefined}
+		<time datetime={date}>{dateFormatter(date, userAgent.language)} UTC</time>
+	{/if}
 </p>
