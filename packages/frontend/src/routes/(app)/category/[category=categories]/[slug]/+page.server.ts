0d63e2bfcec23104a880d3110f7f5adf7b5ffb7b
Jonathan <jacobrownings@gmail.com>
2024-11-04 15:53:53 +0800

    refactor(f): changed query to include relatedArticles
    Changed code in 'page.server.ts' so that it uses the getRecommendedArticles function and returns relatedArticles

--- packages/frontend/src/routes/(app)/category/[category=categories]/[slug]/+page.server.ts
+++ packages/frontend/src/routes/(app)/category/[category=categories]/[slug]/+page.server.ts
@@ -1,6 +1,11 @@
 import { error, type ServerLoadEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { type Article, getOneArticleFromCategory } from '$lib/sanity';
+import {
+	type Article,
+	getOneArticleFromCategory,
+	getOneArticleFromSeries,
+	getRecommendedArticles
+} from '$lib/sanity';
 
 export const load: PageServerLoad = (async (event: ServerLoadEvent) => {
 	const { category, slug } = event.params;
@@ -8,14 +13,24 @@ export const load: PageServerLoad = (async (event: ServerLoadEvent) => {
 		(category as string).toLowerCase(),
 		slug as string
 	);
+
 	const title = article.title;
 
+	let relatedArticles: Article[];
+
 	if (article) {
+		relatedArticles = await getRecommendedArticles(
+			article.series.slug.current as string,
+			article.title as string,
+			article.authors[0].slug.current as string,
+			article.tags[0].slug.current as string,
+			article.category.slug.current as string
+		);
 		return {
 			article,
-			title
+			title,
+			relatedArticles
 		};
 	}
-
 	throw error(404, 'Not found');
 }) satisfies PageServerLoad;
