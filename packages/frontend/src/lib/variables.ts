66cf06842fd46d41e93d0c8070197fd19c55b59f
Neo <99268950+n30w@users.noreply.github.com>
2024-11-11 02:27:57 +0800

    refactor(f): up `variables.ts` with site defaults

--- packages/frontend/src/lib/variables.ts
+++ packages/frontend/src/lib/variables.ts
@@ -1,2 +1,26 @@
 // Release version.
-export const semVer = '0.5.0';
+export const semVer = '0.5.1';
+
+/**
+ * `site` is an object used in various components and server
+ * code that contains metadata defaults. These fields exist in this
+ * variable file because, in the due case a rebrand must happen
+ * or we move campuses again or the defaults of our brand change,
+ * all that needs to happen is a manipulation of these variables!
+ */
+export const site = {
+	url: 'https://onmagnoliasquare.com',
+	locale: 'en-US',
+	name: 'On Magnolia Square',
+	title: 'On Magnolia Square',
+	description: 'On Magnolia Square is a student journalism club at NYU Shanghai.',
+	articleTags: ['student journalism', 'nyu shanghai']
+};
+
+/**
+ * `filler` is an object containing filler defaults—stuff that is
+ * used throughout the code that acts as content defaults.
+ */
+export const filler = {
+	memberDescription: 'is a contributor to On Magnolia Square'
+};
