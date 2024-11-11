351a84f6e6584236cecab2a0f1d2bb5d297ad9a6
Neo <99268950+n30w@users.noreply.github.com>
2024-11-12 04:51:24 +0800

    feat(b): add `fieldGroups`

---
+++ packages/backend/schemaTypes/objects/fieldGroups.ts
@@ -0,0 +1,17 @@
+import {CogIcon, ComposeIcon, InfoOutlineIcon} from '@sanity/icons'
+import {FieldGroupDefinition} from 'sanity'
+
+export const InfoGroup: FieldGroupDefinition = {
+  name: 'info',
+  title: 'Info',
+  default: true,
+  icon: InfoOutlineIcon,
+}
+
+export const ContentGroup: FieldGroupDefinition = {
+  name: 'content',
+  title: 'Content',
+  icon: ComposeIcon,
+}
+
+export const SeoGroup: FieldGroupDefinition = {name: 'seo', title: 'SEO', icon: CogIcon}
