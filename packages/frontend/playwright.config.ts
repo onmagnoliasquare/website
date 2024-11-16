994b8f5b6bfeb52e5bcd1478f9a39b77f9772b8a
Neo <99268950+n30w@users.noreply.github.com>
2024-11-17 05:19:49 +0800

    test(f): up playwright dev server use

--- packages/frontend/playwright.config.ts
+++ packages/frontend/playwright.config.ts
@@ -2,8 +2,11 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'yarn run build && yarn run preview',
-		port: 4173
+		command: 'yarn dev:front',
+		url: 'http://127.0.0.1:5173',
+		reuseExistingServer: !process.env.CI,
+		stdout: 'ignore',
+		stderr: 'pipe'
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
