3a64b5e8b33317eaf6a595b5e79e767169189d88
Neo <99268950+n30w@users.noreply.github.com>
2024-11-19 02:24:24 +0800

    test(f): add playwright basic nav+link tests

--- packages/frontend/tests/test.ts
+++ packages/frontend/tests/test.ts
@@ -1,6 +1,100 @@
-import { expect, test } from '@playwright/test';
+// https://playwright.dev/docs/test-use-options#configuration-scopes
 
-test('index page has expected h1', async ({ page }) => {
+import { test, expect } from '@playwright/test';
+
+test('Homepage has expected h2', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Latest Articles', exact: true })).toBeVisible();
+});
+
+test('About page has expected h1', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+});
+
+test('Staff page is accessible via About page', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+
+	// Click the staff link.
+	await page.getByRole('link', { name: 'Staff' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Staff' })).toBeVisible();
+});
+
+test('Neo Alabastro page has expected h1', async ({ page }) => {
+	await page.goto('/about/staff/neo-alabastro');
+	await expect(page.getByRole('heading', { name: 'Neo Alabastro' })).toBeVisible();
+});
+
+test('News category page has expected h1', async ({ page }) => {
+	await page.goto('/category/news');
+	await expect(page.getByRole('heading', { name: 'News' })).toBeVisible();
+});
+
+test('People category page has expected h1', async ({ page }) => {
+	await page.goto('/category/people');
+	await expect(page.getByRole('heading', { name: 'People', exact: true })).toBeVisible();
+});
+
+test('Opinion page has expected h1', async ({ page }) => {
+	await page.goto('/category/opinion');
+	await expect(page.getByRole('heading', { name: 'Opinion' })).toBeVisible();
+});
+
+test('Culture page has expected h1', async ({ page }) => {
+	await page.goto('/category/culture');
+	await expect(page.getByRole('heading', { name: 'Culture' })).toBeVisible();
+});
+
+test('Series page has expected h1', async ({ page }) => {
+	await page.goto('/series');
+	await expect(page.getByRole('heading', { name: 'Series' })).toBeVisible();
+});
+
+test('Archive page has expected h1', async ({ page }) => {
+	await page.goto('/archive');
+	await expect(page.getByRole('heading', { name: 'Archive' })).toBeVisible();
+});
+
+test('#oncenturyavenue tag accessible via Archive page', async ({ page }) => {
+	await page.goto('/archive');
+	await page.getByRole('link', { name: 'on century avenue' }).click();
+	await expect(page.getByRole('heading', { name: '# on century avenue' })).toBeVisible();
+});
+
+test('#advice tag accessible via Archive page', async ({ page }) => {
+	await page.goto('/archive');
+	await page.getByRole('link', { name: 'advice' }).click();
+	await expect(page.getByRole('heading', { name: '# advice' })).toBeVisible();
+});
+
+test('Category on by line accessible via article page', async ({ page }) => {
+	await page.goto(`/category/news/this-year's-changes-in-cost-of-attendance-and-financial-aid`);
+	await expect(
+		page.getByRole('heading', {
+			name: `This Year's Changes in Cost of Attendance and Financial Aid`
+		})
+	).toBeVisible();
+
+	// Click Category link on the ByLine.
+	await page.getByRole('main').getByRole('link', { name: 'News' }).click();
+
+	await expect(page.getByRole('main').getByRole('heading', { name: 'News' })).toBeVisible();
+});
+
+test('Author accessible via article page', async ({ page }) => {
+	await page.goto(`/category/news/this-year's-changes-in-cost-of-attendance-and-financial-aid`);
+	await expect(
+		page.getByRole('heading', {
+			name: `This Year's Changes in Cost of Attendance and Financial Aid`
+		})
+	).toBeVisible();
+
+	// Click Author link on the ByLine.
+	await page.getByRole('main').getByRole('link', { name: 'Fikret Halilov' }).click();
+
+	await expect(
+		page.getByRole('main').getByRole('heading', { name: 'Fikret Halilov' })
+	).toBeVisible();
 });
