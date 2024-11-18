44d91f1fa95df63af982832c452926e3aca10616
Neo <99268950+n30w@users.noreply.github.com>
2024-11-19 02:08:18 +0800

    refactor(f): up tag page headers to `<h1>`

--- packages/frontend/src/routes/(app)/archive/tags/[tagName]/+page.svelte
+++ packages/frontend/src/routes/(app)/archive/tags/[tagName]/+page.svelte
@@ -16,15 +16,15 @@
 		<div class="dib">
 			{#if data.tag.name == 'on century avenue'}
 				<div class="bg-purple br-pill pa0 pa1-ns ph2">
-					<p class="f6 f3-ns ma0 pa0 pv1 pv0-ns tracked-02 fw6 ph2-ns">
+					<h1 class="sans f6 f3-ns ma0 pa0 pv1 pv0-ns tracked-02 fw6 ph2-ns white">
 						# {data.tag.name}
-					</p>
+					</h1>
 				</div>
 			{:else}
 				<div class="bg-green br-pill pa0 pa1-ns ph2">
-					<p class="f6 f3-ns ma0 pa0 pv1 pv0-ns tracked-02 fw6 ph2-ns">
+					<h1 class="sans f6 f3-ns ma0 pa0 pv1 pv0-ns tracked-02 fw6 ph2-ns white">
 						# {data.tag.name}
-					</p>
+					</h1>
 				</div>
 			{/if}
 		</div>
