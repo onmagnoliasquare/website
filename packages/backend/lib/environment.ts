e41b90575cd086916714e0f698b827f673519e42
Neo <99268950+n30w@users.noreply.github.com>
2024-12-23 19:30:52 -1000

    refactor(b): add env logic

---
+++ packages/backend/lib/environment.ts
@@ -0,0 +1,3 @@
+export const studioProjectId = '1ah7xxlt'
+export const studioTitle = process.env.SANITY_STUDIO_TITLE
+export const studioDataset = process.env.SANITY_STUDIO_DATASET
--- packages/backend/sanity.config.ts
+++ packages/backend/sanity.config.ts
@@ -5,64 +5,28 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {BookIcon, RobotIcon} from '@sanity/icons'
 import {media} from 'sanity-plugin-media'
+import {studioDataset, studioProjectId, studioTitle} from './lib/environment'
 
-export default defineConfig([
-  {
-    name: 'production',
-    title: 'Publishing',
-
-    basePath: '/publish',
-    subtitle: 'Write, edit, and publish content',
-    icon: BookIcon,
-
-    projectId: '1ah7xxlt',
-    dataset: 'production',
-
-    plugins: [
-      structureTool(),
-      media({
-        creditLine: {
-          enabled: true,
-        },
-      }),
-      copyPastePlugin(),
-    ],
-
-    schema: {
-      types: schemaTypes,
-    },
-
-    announcements: {
-      enabled: false,
-    },
+export default defineConfig({
+  name: 'publishing',
+  title: studioTitle,
+  icon: studioDataset! === 'production' ? BookIcon : RobotIcon,
+  projectId: studioProjectId,
+  dataset: studioDataset!,
+  plugins: [
+    structureTool(),
+    visionTool(),
+    media({
+      creditLine: {
+        enabled: true,
+      },
+    }),
+    copyPastePlugin(),
+  ],
+  schema: {
+    types: schemaTypes,
   },
-  {
-    name: 'development',
-    title: 'Development',
-
-    basePath: '/development',
-    subtitle: 'Tech team backend workspace',
-    icon: RobotIcon,
-
-    projectId: '1ah7xxlt',
-    dataset: 'development',
-
-    plugins: [
-      structureTool(),
-      visionTool(),
-      media({
-        creditLine: {
-          enabled: true,
-        },
-      }),
-    ],
-
-    schema: {
-      types: schemaTypes,
-    },
-
-    announcements: {
-      enabled: false,
-    },
+  announcements: {
+    enabled: false,
   },
-])
+})
