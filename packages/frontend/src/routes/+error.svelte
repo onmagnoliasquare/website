e40ef1f8a98066e4e0424d2ed2b549d2d2e3ca58
Neo <99268950+n30w@users.noreply.github.com>
2024-12-24 09:09:06 +0800

    ci: reorder build actions (#208)

--- packages/frontend/src/routes/+error.svelte
+++ packages/frontend/src/routes/+error.svelte
@@ -9,7 +9,6 @@
 	<title>{site.title} – {status} Error</title>
 </svelte:head>
 
-<!-- TODO center this message! -->
 <h1>{status}: {msg}</h1>
 <h2>Looks like something went wrong...</h2>
 <p><a href="/">Click here to go back to the homepage</a></p>
