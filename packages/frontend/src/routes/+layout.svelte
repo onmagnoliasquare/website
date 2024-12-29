55601bc2ae4015e59c6c499216b283d88074876f
Neo <99268950+n30w@users.noreply.github.com>
2024-12-28 19:40:20 -1000

    refactor(f): main layout use tailwind

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	 * ======== ROOT LAYOUT ========
 	 * */
 
-	import { Footer, VersionLabel } from '$lib';
+	import { Header, Footer, VersionLabel } from '$lib';
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
 
@@ -14,6 +14,7 @@
 	import '../app.css';
 	// import '../styles/onmagnoliasquare.css';
 	// import '../styles/typography.css';
+
 	import { dev } from '$app/environment';
 
 	interface Props {
@@ -30,42 +31,45 @@
 
 <header>
 	{#if dev}
-		<div class="w-100 ma0 pa0 bg-red">
+		<div class="w-full m-0 p-0 bg-red-200">
 			<div class="mw9 center ma0 pa0">
 				<div class="flex flex-flow items-center justify-center">
-					<p class="ttu sans-serif fw4 tracked-02 f5 fw8 pa0 ma0 white">
-						⚠️ This is a development environment ️⚠️
+					<p class="ttu fw4 tracked-02 f5 fw8 pa0 ma0 white">
+						⚠️ This is a LOCAL development environment ️⚠️
 					</p>
 				</div>
 			</div>
 		</div>
 	{/if}
-	<div class="w-100 ma0 pa1">
-		<div class="mw9 center ma0 pa0">
-			<div class="flex flex-row-reverse items-center justify-left">
-				{#if dev || import.meta.env.MODE === 'development'}
-					<div class="ph3">
-						<a href={`https://github.com/onmagnoliasquare/website`} target="_blank">
-							<p class="f6 pa0 ma0 font-monospace tracked-tight">dev</p>
-						</a>
-					</div>
-				{:else if import.meta.env.MODE === 'staging'}
-					<div class="ph3">
-						<a href={`https://github.com/onmagnoliasquare/website`} target="_blank">
-							<p class="f6 pa0 ma0 font-monospace tracked-tight">staging</p>
-						</a>
-					</div>
-				{:else}
-					<VersionLabel />
-				{/if}
-			</div>
+	<div class="w-full">
+		<div class="flex flex-row-reverse items-center justify-left p-1">
+			{#if dev || import.meta.env.MODE === 'development'}
+				<div class="py-2">
+					<a href={`https://github.com/onmagnoliasquare/website`} target="_blank">
+						<p class="font-mono text-sm">dev</p>
+					</a>
+				</div>
+			{:else if import.meta.env.MODE === 'staging'}
+				<div class="py-2">
+					<a href={`https://github.com/onmagnoliasquare/website`} target="_blank">
+						<p class="font-mono text-sm">staging</p>
+					</a>
+				</div>
+			{:else}
+				<VersionLabel />
+			{/if}
 		</div>
 	</div>
+	<Header />
 </header>
 
 <main>
-	<div class="mw9 w-100 center pa1 pa3-l">
-		{@render children()}
+	<div
+		class="flex flex-flow overflow-y-clip justify-center items-center mx-4 my-4 sm:mx-10 sm:my-10"
+	>
+		<div class="w-full sm:max-w-7xl">
+			{@render children()}
+		</div>
 	</div>
 </main>
 
