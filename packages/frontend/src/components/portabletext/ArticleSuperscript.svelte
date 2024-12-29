598736140b153d798ddfa0988e742bf6c10d5e83
Neo <99268950+n30w@users.noreply.github.com>
2024-12-28 19:42:36 -1000

    feat(f): add Sub+Super script render support

---
+++ packages/frontend/src/components/portabletext/ArticleSuperscript.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { MarkComponentProps } from '$lib';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		portableText: MarkComponentProps;
+		children?: Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<sup>{@render children?.()}</sup>
