178c2bf808195184a5cf25e10bbea3f30cea29a2
Neo <99268950+n30w@users.noreply.github.com>
2024-12-28 19:42:15 -1000

    refactor(f): portabletext use tailwind

--- packages/frontend/src/components/portabletext/ArticleImage.svelte
+++ packages/frontend/src/components/portabletext/ArticleImage.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import PhotoCaption from '$components/custom/PhotoCaption.svelte';
+	import P from '$components/defaults/P.svelte';
 	import { urlFor } from '$lib/sanity';
 
 	interface Props {
@@ -8,37 +10,28 @@
 	let { portableText }: Props = $props();
 </script>
 
-<figure role="group" class="ma2 mb4 mw6 center">
+<figure role="group" class="max-w-xl ml-auto mr-auto">
 	<img
 		src={urlFor(portableText.value).fit('max').auto('format').url()}
 		alt={portableText.value.alt}
 		loading="lazy"
+		class="mb-2"
 	/>
 	<figcaption>
 		{#if portableText.value.title}
-			<h2 id="fig-title" class="ph1 fw5 i ma0 mb3 mt2 f4">{portableText.value.title}</h2>
+			<h2 class="mb-1 font-bold tracking-wide">{portableText.value.title}</h2>
 		{/if}
 		{#if portableText.value.description || portableText.value.attrs.creditLine}
-			<div class="fw5 f6 gray ph1">
-				<div class="flex flex-column">
-					{#if portableText.value.description}
-						<div>
-							{portableText.value.description}
-						</div>
-					{/if}
-					<div class="mt1">
-						<p class="pa0 ma0 i o-40">
-							Photo credit: {portableText.value.attrs.creditLine}
-						</p>
-					</div>
-				</div>
+			<div class="flex flex-col space-y-1">
+				{#if portableText.value.description}
+					<P class="text-sm">
+						{portableText.value.description}
+					</P>
+				{/if}
+				<PhotoCaption>
+					{portableText.value.attrs.creditLine}
+				</PhotoCaption>
 			</div>
 		{/if}
 	</figcaption>
 </figure>
-
-<style>
-	img {
-		padding-bottom: 0.75rem;
-	}
-</style>
