8b5edb7db667d45a7c9c043325cca0443756081a
Neo <99268950+n30w@users.noreply.github.com>
2025-01-03 22:46:31 -1000

    feat(f): add `HoverDim.svelte`

---
+++ packages/frontend/src/components/general/HoverDim.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	interface Props {
+		class?: string;
+		children: Snippet;
+	}
+
+	let { class: classList, children }: Props = $props();
+</script>
+
+<div class={twMerge('hover:opacity-50', classList)}>
+	{@render children()}
+</div>
