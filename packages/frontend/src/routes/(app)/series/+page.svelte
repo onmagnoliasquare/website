765c7a3ef6958f28501da881edbc0422ce757898
Neo <99268950+n30w@users.noreply.github.com>
2025-01-03 22:52:10 -1000

    refactor(f): up series page

--- packages/frontend/src/routes/(app)/series/+page.svelte
+++ packages/frontend/src/routes/(app)/series/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import PageHeader from '$components/PageHeader.svelte';
-	import NormalCentering from '$components/NormalCentering.svelte';
 	import { CardLink } from '$lib';
 	import type { Series } from '$lib/schema';
 
@@ -13,13 +12,11 @@
 	let series: Series[] = data.series;
 </script>
 
-<NormalCentering>
-	<PageHeader>Series</PageHeader>
-	<ul class="pa0 ma0 list">
-		{#each series as s}
-			<li>
-				<CardLink title={s.name} link={`/series/${s.slug.current}`}>{s.description}</CardLink>
-			</li>
-		{/each}
-	</ul>
-</NormalCentering>
+<PageHeader>Series</PageHeader>
+<ul class="list">
+	{#each series as s}
+		<li>
+			<CardLink title={s.name} link={`/series/${s.slug.current}`}>{s.description}</CardLink>
+		</li>
+	{/each}
+</ul>
