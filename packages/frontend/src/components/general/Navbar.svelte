408112c00867ed7e2e4caf77ddc7d7785ccf68f8
Neo <99268950+n30w@users.noreply.github.com>
2025-01-11 02:50:54 -1000

    refactor(f): remove `id` from navbar

--- packages/frontend/src/components/general/Navbar.svelte
+++ packages/frontend/src/components/general/Navbar.svelte
@@ -4,7 +4,7 @@
 	let listOfRoutes = routes;
 </script>
 
-<nav id="mainNav" class="sm:divide-x-1 sm:border-b-1 p-2 relative hidden sm:block">
+<nav class="sm:divide-x-1 sm:border-b-1 p-2 relative hidden sm:block">
 	<ul class="list-none">
 		{#each listOfRoutes as route}
 			<li class="mr-2 pr-2 tracking-wide hidden sm:inline">
