9f5ef432fc9144aaa5a278ac9c21e3eb8c8a813f
Neo <99268950+n30w@users.noreply.github.com>
2025-01-15 00:55:54 -1000

    feat(f): add `+error` for `(app)` routes

---
+++ packages/frontend/src/routes/(app)/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { site } from '$lib/variables';
+	export const msg = $page.error!.message;
+	export const status = $page.status;
+</script>
+
+<svelte:head>
+	<title>{status} Error – {site.title}</title>
+</svelte:head>
+
+<div class="flex flex-col min-h-screen justify-center items-center">
+	<h1 class="text-6xl font-serif font-bold select-none">{status}</h1>
+	<p class="text-xl font-serif font-semibold mt-2 pt-2">{msg}</p>
+	<p class="mt-1 pt-1">Looks like something went wrong...</p>
+	<p class="mt-2 pt-2"><a href="/">Click here to go back to the homepage</a></p>
+</div>
