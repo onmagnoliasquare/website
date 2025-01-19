c7c1b638a1113406b88ea8433d75b0bf6b110041
Neo <99268950+n30w@users.noreply.github.com>
2025-01-18 23:48:55 -1000

    Organize playwright tests and workflow perms (#246)

--- packages/frontend/tests/testVariables.ts
+++ packages/frontend/tests/testVariables.ts
@@ -5,3 +5,4 @@ export const v05TestArticleUrl = `/category/news/v05-article-feature-set--what-a
 export const v05DummyDataPath = '../dummyData/v5/data.json';
 export const v05Category = 'news';
 export const v05Slug = 'v05-article-feature-set--what-a-blast';
+export const authorUrl = `/about/staff/neo-alabastro`;
