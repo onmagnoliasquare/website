b215755bee6bb626c08a29609884dfecb1e04fac
Neo <99268950+n30w@users.noreply.github.com>
2025-02-02 16:55:52 +0800

    feat(f): add aria-label to nav

--- packages/frontend/src/components/general/MobileNavbar.svelte
+++ packages/frontend/src/components/general/MobileNavbar.svelte
@@ -4,23 +4,25 @@
 	let { toggleNavbar, showMenu } = $props();
 </script>
 
-<ul
-	class="list-none flex-col md:mt-8 space-y-4 md:space-y-0 sm:hidden w-full {showMenu
-		? 'overscroll-contain border-b-1 pb-4 mt-4'
-		: 'hidden'}"
->
-	{#each listOfRoutes as route}
-		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-		<!-- svelte-ignore a11y_click_events_have_key_events -->
-		<li
-			onclick={() => {
-				toggleNavbar();
-			}}
-			class="tracking-wide w-fit ml-2 pl-2 hover:underline"
-		>
-			<a href={route.path} title={route.name} class="hover:underline">
-				{route.name}
-			</a>
-		</li>
-	{/each}
-</ul>
+<nav aria-label="Mobile Site Menu">
+	<ul
+		class="list-none flex-col md:mt-8 space-y-4 md:space-y-0 sm:hidden w-full {showMenu
+			? 'overscroll-contain border-b-1 pb-4 mt-4'
+			: 'hidden'}"
+	>
+		{#each listOfRoutes as route}
+			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<li
+				onclick={() => {
+					toggleNavbar();
+				}}
+				class="tracking-wide w-fit ml-2 pl-2 hover:underline"
+			>
+				<a href={route.path} title={route.name} class="hover:underline">
+					{route.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
