7a039c5190edeb0bd6dd7fc66bf5a58e64b386fd
Neo <99268950+n30w@users.noreply.github.com>
2025-02-09 01:12:16 +0800

    feat: add `flake.nix` and `.envrc`

---
+++ flake.nix
@@ -0,0 +1,47 @@
+# modified from https://github.com/akirak/flake-templates/blob/master/node-typescript/flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        commonPackages = with pkgs; [
+          # Development related
+          nodejs_22
+          typescript
+          typescript-language-server
+
+          # System tools
+          lazygit
+          htop
+          mprocs
+        ];
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ commonPackages ];
+          shellHook = ''
+               # Customize the prompt to show we're in a Nix environment
+            export PS1='$(printf "\033[01;34m(nix) \033[00m\033[01;32m[%s] \033[01;33m(node $(node -v))\033[00m$\033[00m " "\W")'
+          '';
+        };
+      }
+    );
+}
