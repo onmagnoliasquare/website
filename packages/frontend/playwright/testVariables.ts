7ad0922a4796220b94b5718d2b1c4f928a19c136
Neo <99268950+n30w@users.noreply.github.com>
2025-03-29 02:26:07 +0800

    refactor: test var organizations

--- packages/frontend/playwright/testVariables.ts
+++ packages/frontend/playwright/testVariables.ts
@@ -1,8 +1,17 @@
 // article404 is an article that shouldn't exist used for 404 page checks.
 export const article404 = `/category/news/asdlkjafsjklfegnjkasjfaasldkjfj2093q580`;
 
+// Globals
+export const authorUrl = `/about/staff/neo-alabastro`;
+
+// v0.5.x
 export const v05TestArticleUrl = `/category/news/v05-article-feature-set--what-a-blast`;
 export const v05DummyDataPath = '../dummyData/v5/data.json';
 export const v05Category = 'news';
 export const v05Slug = 'v05-article-feature-set--what-a-blast';
-export const authorUrl = `/about/staff/neo-alabastro`;
+
+// v0.6.x
+export const v06TestArticleUrl = ``;
+export const v06DummyDataPath = ``;
+export const v06Category = 'news';
+export const v06Slug = ``;
