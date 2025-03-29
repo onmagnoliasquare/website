5620aed31d6dda63f35d18dc3ad82a87391fc243
Neo <99268950+n30w@users.noreply.github.com>
2025-03-29 21:06:33 +0800

    test(f): organize imports

--- packages/frontend/playwright/testVariables.ts
+++ packages/frontend/playwright/testVariables.ts
@@ -6,12 +6,38 @@ export const authorUrl = `/about/staff/neo-alabastro`;
 
 // v0.5.x
 export const v05TestArticleUrl = `/category/news/v05-article-feature-set--what-a-blast`;
-export const v05DummyDataPath = '../dummyData/v5/data.json';
+export const v05DummyDataPath = '../dummyData/v5/article.json';
 export const v05Category = 'news';
 export const v05Slug = 'v05-article-feature-set--what-a-blast';
 
 // v0.6.x
-export const v06TestArticleUrl = ``;
-export const v06DummyDataPath = ``;
+export const v06TestArticleUrl = `/category/news/v06-features`;
+export const v06DummyDataPath = `../dummyData/v6/article.json`;
 export const v06Category = 'news';
-export const v06Slug = ``;
+export const v06Slug = `v06-features`;
+
+export interface articleTestData {
+	title: string;
+	dataPath: string;
+	category: string;
+	slug: string;
+	url: string;
+}
+
+export const v0_5Article: articleTestData = {
+	title: 'v0.5.x',
+	dataPath: `../dummyData/v0.5.x/article.json`,
+	category: 'news',
+	slug: 'v05-article-feature-set--what-a-blast',
+	url: '/category/news/v05-article-feature-set--what-a-blast'
+};
+
+export const v0_6Article: articleTestData = {
+	title: 'v0.6.x',
+	dataPath: `../dummyData/v0.6.x/article.json`,
+	category: 'news',
+	slug: 'v06-features',
+	url: '/category/news/v06-features'
+};
+
+export const articleTestDataList: Array<articleTestData> = [v0_5Article, v0_6Article];
