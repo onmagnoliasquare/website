15bcb1bda37a405c3926f1768df9ae3336554aa5
Neo <99268950+n30w@users.noreply.github.com>
2025-04-12 04:42:24 +0800

    feat: add `studioApiVersion` to env

--- packages/backend/lib/environment.ts
+++ packages/backend/lib/environment.ts
@@ -1,3 +1,4 @@
 export const studioProjectId = '1ah7xxlt'
 export const studioTitle = process.env.SANITY_STUDIO_TITLE
 export const studioDataset = process.env.SANITY_STUDIO_DATASET
+export const studioApiVersion = 'v2025-04-12'
