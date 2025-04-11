69222188116c766425052050807bd03f169d59a5
Neo <99268950+n30w@users.noreply.github.com>
2025-04-12 06:24:18 +0800

    refactor(b): organize `structure`

---
+++ packages/backend/lib/defineStructure.ts
@@ -0,0 +1,15 @@
+/**
+ * Modified from:
+ * https://github.com/sanity-io/sanity-shopify-studio/blob/main/utils/defineStructure.ts
+ */
+import {ConfigContext} from 'sanity'
+import {StructureBuilder} from 'sanity/structure'
+
+/**
+ * Helper for creating and typing composable desk structure parts.
+ */
+export default function defineStructure<StructureType>(
+  factory: (S: StructureBuilder, context: ConfigContext) => StructureType,
+) {
+  return factory
+}
