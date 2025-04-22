83763bf7ee074ca0c45352f6060817ea56b36ce9
Neo <99268950+n30w@users.noreply.github.com>
2025-04-22 18:22:50 +0800

    refactor: add detailed article api fetch

--- packages/frontend/src/routes/api/article/+server.ts
+++ packages/frontend/src/routes/api/article/+server.ts
@@ -1,11 +1,11 @@
-import { buildSanityQuery, equal, sanityFetch } from '$lib/sanity';
-import type { ApiError, Article } from '$lib/schema';
 import { dev } from '$app/environment';
+import { fetchArticlePage } from '$lib/sanity/repository';
+import type { ApiError } from '$lib/schema';
+import type { DetailedArticleQueryResult } from '$lib/types/api';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ url }) => {
-	let sanityQuery: string;
-	let article: Article | undefined;
+	let article: DetailedArticleQueryResult | undefined;
 
 	const category = url.searchParams.get('category');
 	const slug = url.searchParams.get('slug');
@@ -16,67 +16,10 @@ export const GET: RequestHandler = async ({ url }) => {
 	} else if (!category && slug) {
 		const error: ApiError = { message: 'Malformed parameter(s)', status: 400 };
 		return json({ error: error }, { status: 400 });
-		// // Request for a single article by it's slug is requested.
-		// try {
-		// 	sanityQuery = buildSanityQuery({
-		// 		type: 'article',
-		// 		idx: [0],
-		// 		conditions: [equal('slug.current', slug as string)],
-		// 		customAttrs: ['tags[]->{name, slug}', 'category->{name, slug}']
-		// 	});
-
-		// 	article = await sanityFetch(sanityQuery);
-		// } catch (err) {
-		// 	console.error(err);
-		// 	return json(
-		// 		{ message: 'Failed to fetch article', error: (err as Error).message },
-		// 		{ status: 500 }
-		// 	);
-		// }
 	} else if (category && slug) {
 		// Request for an article page.
 		try {
-			sanityQuery = buildSanityQuery({
-				type: 'article',
-				idx: [0],
-				conditions: [
-					equal('category->slug.current', (category as string).toLowerCase()),
-					equal('slug.current', slug as string)
-				],
-				attributes: [
-					'title',
-					'subtitle',
-					'date',
-					'media',
-					'updatedDate',
-					'metaInfo',
-					'slug',
-					'_id'
-				],
-				customAttrs: [
-					`content[]{
-						_type == "image" => {
-							title,
-							alt,
-							description,
-							"attrs": asset-> {
-								creditLine,
-								metadata
-							},
-						},
-						...
-					}`,
-					'authors[]->{_id, name, slug}',
-					'tags[]->{name, slug}',
-					'category->{_id, name, slug}',
-					`"asset": media.asset->{creditLine, metadata}`,
-					'series->{name, slug}' // Below query modified from:
-					// https://www.sanity.io/schemas/get-related-items-of-a-post-in-sanity-by-comparing-category-array-reference-with-another-array-0d752dd7
-					// `"related": *[_type == "article" && category._ref == ^.category._ref] | order(date) [0..5]`
-				]
-			});
-
-			article = await sanityFetch(sanityQuery);
+			article = await fetchArticlePage(slug, category);
 		} catch (err) {
 			if (dev) {
 				console.error(err);
