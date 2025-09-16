ebbfe097dc7b6b730c47af299f88be8b4ef9665e
Neo <99268950+n30w@users.noreply.github.com>
2025-09-15 22:28:26 -1000

    chore: use mjs for prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,94 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  useTabs: false,
+  endOfLine: 'auto',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'strict',
+  bracketSpacing: true,
+  insertPragma: false,
+  requirePragma: false,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  parser: 'babel',
+  jsxSingleQuote: false,
+  vueIndentScriptAndStyle: false,
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  plugins: [
+    'prettier-plugin-svelte',
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteBracketNewLine: false,
+  svelteAllowShorthand: false,
+  svelteIndentScriptAndStyle: false,
+  importOrder: ['^@lib/(.*)$', '', '^@components/(.*)$', '', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+  importOrderCaseSensitive: false,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        parser: 'html',
+      },
+    },
+    {
+      files: '*.css',
+      options: {
+        parser: 'css',
+      },
+    },
+    {
+      files: '*.{js,mjs,cjs}',
+      options: {
+        parser: 'babel',
+      },
+    },
+    {
+      files: '*.ts',
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: '*.tsx',
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte',
+      },
+    },
+    {
+      files: '*.json',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.{yaml,yml}',
+      options: {
+        parser: 'yaml',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+      },
+    },
+  ],
+}
+
+export default config
