13ac5f94e6b4f654a0ffd9807d31520c8a344429
Neo <99268950+n30w@users.noreply.github.com>
2025-09-26 00:57:33 -1000

    test(f): fix vitest resolution

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -1,22 +1,33 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config.ts'
+import type { UserConfig } from 'vite'
 
-export default defineConfig({
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    environment: 'node',
-    setupFiles: ['./vitest-setup.js'],
-    restoreMocks: true,
-    pool: 'forks',
-    isolate: false,
-    poolOptions: {
-      forks: {
-        singleFork: true,
+// For how I created this setup, see the link below.
+// See: https://github.com/vitest-dev/vitest/discussions/3042#discussioncomment-6449044
+
+const config = mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      ...configDefaults,
+      include: ['src/**/*.{test,spec}.{js,ts}'],
+      environment: 'node',
+      setupFiles: ['./vitest-setup.js'],
+      restoreMocks: true,
+      pool: 'forks',
+      isolate: false,
+      poolOptions: {
+        forks: {
+          singleFork: true,
+        },
+      },
+      server: {
+        deps: {
+          inline: [`@sveltejs/kit`],
+        },
       },
     },
-    server: {
-      deps: {
-        inline: [`@sveltejs/kit`],
-      },
-    },
-  },
-})
+  }) as UserConfig
+)
+
+export default config
