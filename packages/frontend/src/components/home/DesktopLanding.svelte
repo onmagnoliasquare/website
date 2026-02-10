fa3a6cd57d47fca0586f52f4cb5146641c586367
Olivia Moody <owymoody@gmail.com>
2026-02-09 14:30:56 -1000

    feat: implement enhanced-img with supporting dependency updates
    # Conflicts:
    #	packages/frontend/vite.config.ts
    #	yarn.lock

--- packages/frontend/src/components/home/DesktopLanding.svelte
+++ packages/frontend/src/components/home/DesktopLanding.svelte
@@ -4,12 +4,13 @@
 /* As well as:  */
 /* https://modern-fluid-typography.vercel.app/ */
 
-#bigText {
-  font-family: var(--font-display), serif;
-  font-size: clamp(8.563rem, 4vw + 2.5rem, 12.813rem);
-  line-height: clamp(8.563rem, 4vw + 2.5rem, 12.813rem);
-  font-weight: 200;
-}
+    #bigText {
+        font-family: var(--font-display), serif;
+        font-size: clamp(6.423rem, 3vw + 1.875rem, 9.612rem);
+        /* line-height: clamp(4.819rem, 2.25vw + 1.4rem, 7.211rem); */
+        line-height: clamp(6.423rem, 3vw + 1.875rem, 9.612rem);
+        font-weight: 200;
+    }
 
 #kindaBigText {
   font-family: var(--font-serif), serif;
@@ -34,12 +35,13 @@
 </style>
 
 <script lang="ts">
-import { createAuthorString } from '$lib/helpers'
-import type { HomepageArticleQueryResult } from '$lib/sanity/types.generated'
-import { site } from '$lib/constants'
-import DateLine from './DateLine.svelte'
-import DesktopLandingNavbar from './DesktopLandingNavbar.svelte'
-import type { Get } from '@sanity/codegen'
+	import { createAuthorString } from '$lib/helpers';
+	import { site } from '$lib/constants';
+	import DateLine from './DateLine.svelte';
+	import DesktopLandingNavbar from './DesktopLandingNavbar.svelte';
+  import oms_logo from '$lib/assets/oms_logo.png';
+	import type { Get } from '@sanity/codegen'
+	import type { HomepageArticleQueryResult } from '$lib/sanity/types.generated';
 
 interface Props {
   article: Get<HomepageArticleQueryResult, number>
@@ -51,50 +53,53 @@ let { article, locale }: Props = $props()
 let headlineArticle = $derived(article)
 let authorString = $derived(createAuthorString(article.authors))
 
-const splitTitle = site.title.split(' ')
 </script>
 
-<div class="min-h-screen grid grid-rows-1 grid-cols-7">
-  <div class="flex flex-col space-y-2 col-span-3 size-fit row-span-1 center">
-    <div class="size-fit p-4">
-      {#each splitTitle as word (word)}
-        <h1 id="bigText" class="font-stretch-condensed">
-          {word}
-        </h1>
-      {/each}
-    </div>
-    <div class="size-fit p-2 m-2">
-      <p class="text-2xl font-display font-stretch-condensed text-neutral-700 font-semibold pt-5">
-        Student journalism and writing at NYU Shanghai
-      </p>
-    </div>
-  </div>
-  <div class="hidden md:block col-span-3 col-start-5 ml-auto row-span-1 p-6">
-    <div id="kindaBigText" class="size-fit p-4 border-2">
-      <p>上海纽约</p>
-      <p>大学官方</p>
-      <p>学生媒体</p>
-    </div>
-  </div>
-  <div class="col-span-7">
-    <DesktopLandingNavbar />
-  </div>
-  <div
-    class="size-full col-span-7 row-span-1 bg-white border-t flex flex-col p-4 items-center text-black">
-    <p id="quip" class="text-center font-display p-4 text-2xl border border-dashed w-fit">
-      The latest scoop by <span class="italic font-semibold">{authorString}</span>
-      on
-      <em>
-        <DateLine locale={locale} date={headlineArticle.date} />
-      </em>
-    </p>
-    <a
-      data-testid="headline-article"
-      href="category/{headlineArticle.category.slug}/{headlineArticle.slug}"
-      class="border-b border-dotted">
-      <h2 class="text-center font-serif text-6xl font-stretch-condensed font-bold p-4">
-        {headlineArticle.title}
-      </h2>
-    </a>
-  </div>
+<div class="h-fit grid grid-rows-1 grid-cols-7">
+	<div class="flex flex-col col-span-5 col-start-2 row-start-1 mt-15 space-y-2 size-fit row-span-1 center">
+		<div class="size-fit p-4 center">
+      <a href="/" class="cursor-pointer">
+				<h1 id="bigText" class="font-stretch-condensed">
+          {site.title}
+				</h1>
+      </a>
+		</div>
+    <p class="size-fit py-2 text-2xl font-display font-stretch-condensed text-neutral-700 font-semibold pt-5 center"> Student journalism and writing at NYU Shanghai </p>
+	</div>
+    <enhanced:img class="size-fit py-2 m-2" src={oms_logo} alt="fleurs" />
+		<!-- <div class="size-fit py-2 mr-auto ml-4 row-start-1 col-span-1 col-start-1 text-2xl font-display font-stretch-condensed text-neutral-700 font-semibold pt-5"> -->
+		<!-- 	<p> Student journalism </p> -->
+		<!--     <p>  and writing </p> -->
+		<!--     <p> at NYU Shanghai </p> -->
+		<!-- </div> -->
+	<div class="hidden md:block col-span-1 col-start-7 row-start-1 ml-auto row-span-1 p-6">
+		<div id="kindaBigText" class="size-fit p-4 border-2">
+			<p>上海纽约</p>
+			<p>大学官方</p>
+			<p>学生媒体</p>
+		</div>
+	</div>
+	<div class="col-span-7">
+		<DesktopLandingNavbar />
+	</div>
+	<div
+		class="size-full col-span-7 row-span-1 bg-white border-t flex flex-col p-4 items-center text-black"
+	>
+		<p id="quip" class="text-center font-display p-4 text-2xl border border-dashed w-fit">
+			The latest scoop by <span class="italic font-semibold">{authorString}</span>
+			on
+			<em>
+				<DateLine locale={locale} date={headlineArticle.date} />
+			</em>
+		</p>
+		<a
+			data-testid="headline-article"
+			href="category/{headlineArticle.category.name.toLowerCase()}/{headlineArticle.slug}"
+			class="border-b border-dotted"
+		>
+			<h2 class="text-center font-serif text-6xl font-stretch-condensed font-bold p-4">
+				{headlineArticle.title}
+			</h2>
+		</a>
+	</div>
 </div>
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), enhancedImages(), sveltekit(), svelteTesting({ autoCleanup: true })],
   define: {
     __ONMAGNOLIASQUARE_FRONTEND_VERSION__: `"${pkg.version}"`,
+  },
+  optimizeDeps: {
+    exclude: [
+      '@portabletext/svelte',
+      '@unpic/svelte',
+      'svelte-meta-tags'
+    ]
   },
 })
