865af595890942ba765e7ec3b9f245aa28dd1b59
Neo <99268950+n30w@users.noreply.github.com>
2026-08-11 06:27:45 -1000

    chore: remove svelteTesting from vite.config.ts

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,20 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { enhancedImages } from '@sveltejs/enhanced-img';
+import { enhancedImages } from '@sveltejs/enhanced-img'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
-  plugins: [tailwindcss(), enhancedImages(), sveltekit(), svelteTesting({ autoCleanup: true })],
+  plugins: [tailwindcss(), enhancedImages(), sveltekit()],
   define: {
     __ONMAGNOLIASQUARE_FRONTEND_VERSION__: `"${pkg.version}"`,
   },
   optimizeDeps: {
-    exclude: [
-      '@portabletext/svelte',
-      '@unpic/svelte',
-      'svelte-meta-tags'
-    ]
+    exclude: ['@portabletext/svelte', '@unpic/svelte', 'svelte-meta-tags'],
   },
 })
