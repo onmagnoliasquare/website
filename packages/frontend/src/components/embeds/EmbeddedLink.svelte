bb8020a18a57fdb12bbafbe4cc8f57cfa36ab34e
Neo <99268950+n30w@users.noreply.github.com>
2026-09-08 21:45:43 -1000

    chore(f): sveltify some components

--- packages/frontend/src/components/embeds/EmbeddedLink.svelte
+++ packages/frontend/src/components/embeds/EmbeddedLink.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import { parseEmbedLink } from '$lib/helpers';
-	import type { CustomBlockComponentProps } from '@portabletext/svelte';
-	import SpotifyEmbed from './SpotifyEmbed.svelte';
+import { parseEmbedLink } from '$lib/helpers';
+import type { CustomBlockComponentProps } from '@portabletext/svelte';
+import SpotifyEmbed from './SpotifyEmbed.svelte';
 import type { EmbeddedLink } from '$lib/sanity/types.generated'
 
-	interface Props {
-		portableText: CustomBlockComponentProps<EmbeddedLink>;
-	}
+interface Props {
+	portableText: CustomBlockComponentProps<EmbeddedLink>;
+}
 
-	let { portableText }: Props = $props();
+let { portableText }: Props = $props();
 
-	let link = $derived(parseEmbedLink(portableText.value.contentUrl));
+let link = $derived(parseEmbedLink(portableText.value.contentUrl));
 </script>
 
 <div class="my-1 py-2">
